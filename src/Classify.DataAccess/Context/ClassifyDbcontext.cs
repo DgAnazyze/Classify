6890da78b4fe83f45142ad1f8ab60f19603dce01
Muxammad <muxammadcodes@gmail.com>
2023-05-03 14:09:38 +0500

    Updates

--- src/Classify.DataAccess/Context/ClassifyDbcontext.cs
+++ src/Classify.DataAccess/Context/ClassifyDbcontext.cs
@@ -1,5 +1,11 @@
-﻿namespace Classify.DataAccess.Context;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class ClassifyDbcontext
+namespace Classify.DataAccess.Context;
+
+public class ClassifyDbcontext : DbContext
 {
+    public ClassifyDbcontext(DbContextOptions<ClassifyDbcontext> options) : base(options)
+    {
+        
+    }
 }
