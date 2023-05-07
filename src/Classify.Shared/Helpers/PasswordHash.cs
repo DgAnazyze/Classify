908c13015ebc8ff0cdc499298dda69a71c469305
Muxammad <muxammadcodes@gmail.com>
2023-05-07 16:48:58 +0500

    helper

--- src/Classify.Shared/Helpers/PasswordHash.cs
+++ src/Classify.Shared/Helpers/PasswordHash.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Classify.Shared.Helpers
+﻿namespace Classify.Shared.Helpers
 {
-    internal class PasswordHash
+    public class PasswordHash
     {
+        public class PasswordHelper
+        {
+            /// <summary>
+            /// Hash password 
+            /// </summary>
+            /// <param name="password"></param>
+            /// <returns></returns>
+            public static string Hash(string password)
+            {
+                return BCrypt.Net.BCrypt.HashPassword(password);
+            }
+
+            /// <summary>
+            /// Verify its correct
+            /// </summary>
+            /// <param name="password"></param>
+            /// <param name="passwordHash"></param>
+            /// <returns></returns>
+            public static bool Verify(string password, string passwordHash)
+            {
+                return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+            }
+        }
     }
 }
