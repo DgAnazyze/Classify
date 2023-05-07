416704cc14c0cc0ad8797a9eda41d5800cae1647
AlJavhar <My274737@gmail.com>
2023-05-07 15:33:01 +0500

    Db fojia

---
+++ src/Classify.DataAccess/Migrations/20230507101902_StudentMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Classify.DataAccess.Migrations
+{
+    public partial class StudentMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Grade = table.Column<short>(type: "smallint", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    LastName = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    MidlleName = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    BirthCertificateSeria = table.Column<string>(type: "text", nullable: true),
+                    BirthCertificateNumber = table.Column<string>(type: "text", nullable: true),
+                    PassportSeria = table.Column<string>(type: "text", nullable: true),
+                    PassportNumber = table.Column<string>(type: "text", nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    Region = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    School = table.Column<string>(type: "text", nullable: true),
+                    Bearings = table.Column<string>(type: "text", nullable: true),
+                    Language = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
