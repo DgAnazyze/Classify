4f3a72e89194a6f1f684b59b913e9a16a733de9f
Muxammad <muxammadcodes@gmail.com>
2023-05-07 17:33:40 +0500

    AdministratorForUpdateDto

--- src/Classify.Service/DTOs/Administrator/AdministratorForUpdateDto.cs
+++ src/Classify.Service/DTOs/Administrator/AdministratorForUpdateDto.cs
@@ -1,7 +1,22 @@
-﻿namespace Classify.Service.DTOs.Administrator
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classify.Service.DTOs.Administrator
 {
     public class AdministratorForUpdateDto
     {
-
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Surname { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string Address { get; set; } = string.Empty;
     }
 }
