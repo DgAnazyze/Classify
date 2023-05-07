1300fdcc7b4c31bc6238ed583e1002a9296f05b7
Muxammad <muxammadcodes@gmail.com>
2023-05-07 16:27:36 +0500

    dtos

--- src/Classify.Service/DTOs/Users/UserForResultDto.cs
+++ src/Classify.Service/DTOs/Users/UserForResultDto.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Classify.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Classify.Service.DTOs.Users
 {
-    internal class UserForResultDto
+    public class UserForResultDto
     {
+        public long Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.SchoolAdmin;
     }
 }
