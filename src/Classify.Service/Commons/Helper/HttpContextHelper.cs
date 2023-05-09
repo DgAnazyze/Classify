1441c5196fc55643b70ff0a1ff5c0772540ef731
Muxammad <muxammadcodes@gmail.com>
2023-05-09 23:43:10 +0500

    Http context helper

--- src/Classify.Service/Commons/Helper/HttpContextHelper.cs
+++ src/Classify.Service/Commons/Helper/HttpContextHelper.cs
@@ -1,5 +1,16 @@
-﻿namespace Classify.Service.Commons.Helper;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Classify.Service.Commons.Helper;
 
 public class HttpContextHelper
 {
+
+    public IHttpContextAccessor Accessor { get; set; }
+    public HttpContext HttpContext => Accessor?.HttpContext;
+
+    public long? Id => long.Parse(HttpContext?.User?.FindFirst("Id").Value);
+    public string Role => HttpContext.User.FindFirst("Role").Value;
+
+    // You may also add here Region when you need
+
 }
